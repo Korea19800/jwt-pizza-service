eaf9db80db62384af34d530834ca877d19845c2e
KangJin Kim <korea19800@gmail.com>
2025-04-04 13:36:54 -0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-//import globals from 'globals';
-//import pluginJs from '@eslint/js';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 
-//export default [
-  //{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  //{ languageOptions: { globals: globals.node } },
-  //{ languageOptions: { globals: globals.jest } },
-  //pluginJs.configs.recommended,
-// ];
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: globals.jest } },
+  pluginJs.configs.recommended,
+];
