911486cc3099b5982d446591021807542b622240
KangJin Kim <korea19800@gmail.com>
2025-04-04 13:30:01 -0600

    Update eslint.config.mjs for deliv11

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,28 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
+
+//export default [
+  //{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  //{ languageOptions: { globals: globals.node } },
+  //{ languageOptions: { globals: globals.jest } },
+  //pluginJs.configs.recommended,
+// ];
+
+// updated eslint.config.mjs for deliv11
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.node } },
-  { languageOptions: { globals: globals.jest } },
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: 2020,
+    },
+    env: {
+      node: true,
+      es2021: true,
+    },
+    rules: {
+      // 여기에 룰들 추가
+    },
+  },
 ];
